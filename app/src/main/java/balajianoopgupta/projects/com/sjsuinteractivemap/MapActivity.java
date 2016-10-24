64658aeee80a52291036c8da98b913e08e06e4c2
balajianoopgupta <balajianoopgupta.ba@gmail.com>
2016-10-23 17:08:17 -0700

    Removed unused functions

--- app/src/main/java/balajianoopgupta/projects/com/sjsuinteractivemap/MapActivity.java
+++ app/src/main/java/balajianoopgupta/projects/com/sjsuinteractivemap/MapActivity.java
@@ -149,19 +149,4 @@ public class MapActivity extends AppCompatActivity {
             searchBuilding(query);
         }
     }
-
-
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        // MotionEvent object holds X-Y values
-//        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-//            String text = "You click at x = " + event.getX() + " and y = " + event.getY();
-//            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
-//        }
-//
-//        return super.onTouchEvent(event);
-//    }
-
-
 }
