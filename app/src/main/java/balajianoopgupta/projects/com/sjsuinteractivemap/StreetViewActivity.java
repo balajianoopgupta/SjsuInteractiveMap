300b98bf7f7a39b3625c87a63f1fb436c5e4d7b9
balaji.byrandurga <balaji.byrandurga@servicenow.com>
2016-10-30 16:05:34 -0700

    Street View Functionality

---
+++ app/src/main/java/balajianoopgupta/projects/com/sjsuinteractivemap/StreetViewActivity.java
@@ -0,0 +1,63 @@
+package balajianoopgupta.projects.com.sjsuinteractivemap;
+
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class StreetViewActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
+    double[] location;
+
+    Double lat;
+    Double lng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_street_view);
+
+        Intent i = getIntent();
+        location = i.getExtras().getDoubleArray("Location");
+        lat = location[0];
+        lng = location[1];
+
+        Log.i("Street","Lat: "+String.valueOf(lat));
+        Log.i("Street","Lng: "+String.valueOf(lng));
+
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+
+        streetViewPanorama.setPosition(new LatLng(lat, lng));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
